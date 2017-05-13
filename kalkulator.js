8628cd59c3aa49ac31094bad703bb1994bc07fb1
ivan primaturangga <primaturangga21@gmail.com>
2017-05-13 15:20:36 +0800

    Added calculator

---
+++ kalkulator.js
@@ -0,0 +1,36 @@
+var arr = ['myName', 21, 'blue'];
+
+arr.forEach(function(item, i){
+	console.log("index ke "+ i + ' = ' + item);
+});
+
+var count = {
+	counter : 2,
+	bil : function(numb){
+		return this.counter + numb;
+	},
+	bil2 : function(numb){
+		return this.counter * numb;
+	},
+	bil3 : function(numb){
+		return this.counter / numb;
+	},
+	bil4 : function(numb){
+		return this.counter - numb;
+	},
+	bil5 : function(numb){
+		if (this.counter < numb) {
+			return 'bilangan a kurang dari bilangan b';
+		}else if(this.counter == numb){
+			return 'bilangan sama dengan'
+		}else{
+			return 'bilangan a lebih dari bilangan b';
+		}
+	}
+};
+
+console.log('(+) '+count.bil(1));
+console.log('(*) '+count.bil2(1));
+console.log('(/) '+count.bil3(1));
+console.log('(-) '+count.bil4(1));
+console.log(count.bil5(1));
